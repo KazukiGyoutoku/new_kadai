4caa3cf926f0d630becfd30ea11c924e9cfed799
”名前” <>
2019-11-02 03:21:56 +0000

    PHP/Laravel 09 Routingについて理解するの課題です

--- web.php
+++ web.php
@@ -9,16 +9,23 @@
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-
 */
+
 Route::group(['prefix' =>'admin'], function(){
-    Route::get('news/create','Admin\NewsController@add');
+    Route::get('news/create'.'NewsController@add');
+    //
+    Route::get('profile/create'.'Admin\ProfileController@add');
+    Route::get('profile/edit'.'Admin\ProfileController@edit');
+    
 });
+    /* prefixパラメータでadminディレクトリを纏め無名関数functionのの設定を
+    'prefix =>admin'に適用させる。
+    Route::getの記述に関しては、指定したアクセスに対して、コントローラーの
+    Actionにそのデータを渡す。
+    
+    例)
+    Route::get('profile/create'.'Admin\ProfileController@add');
+    
+    createにアクセスされた時、Admin\ProfileControllerのadd関数にアクセスを渡す。*/
 
 
-//課題4
-
-Route::group(['prefix' => 'admin'],function(){
-    Route::get('admin/profile/create'.'Admin.ProfileController@add');
-    Route::get('admin/profile/edit'.'Admin.ProfileController@edit');
-});
